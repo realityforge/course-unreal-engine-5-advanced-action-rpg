437c386a43d5d9ebedaa2c708793842cda89fccf
Peter Donald <peter@realityforge.org>
2024-07-31 09:02:19 +1000

    Make method const

--- Source/Warrior/Private/DataAssets/Input/DataAsset_InputConfig.cpp
+++ Source/Warrior/Private/DataAssets/Input/DataAsset_InputConfig.cpp
@@ -1,6 +1,6 @@
 #include "DataAssets/Input/DataAsset_InputConfig.h"
 
-UInputAction* UDataAsset_InputConfig::FindNativeInputActionByTag(const FGameplayTag& InInputTag)
+UInputAction* UDataAsset_InputConfig::FindNativeInputActionByTag(const FGameplayTag& InInputTag) const
 {
     for (const auto& Config : NativeInputActions)
     {
