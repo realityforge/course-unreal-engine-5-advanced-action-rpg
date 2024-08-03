7b954a0a0fd6ca6242f0f466cdcaee3249937a60
Peter Donald <peter@realityforge.org>
2024-08-03 22:57:27 +1000

    Start to add a new set of "Ability"s that can be granted to hero

--- Source/Warrior/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
+++ Source/Warrior/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
@@ -1,1 +1,30 @@
 #include "DataAssets/StartUpData/DataAsset_HeroStartUpData.h"
+#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
+
+bool FWarriorHeroAbilitySet::IsValid() const
+{
+    return Tag.IsValid() && Ability;
+}
+
+void UDataAsset_HeroStartUpData::GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InAbilitySystemComponent,
+                                                              const int32 ApplyLevel)
+{
+    Super::GiveToAbilitySystemComponent(InAbilitySystemComponent, ApplyLevel);
+
+    for (const auto& AbilitySet : StartUpAbilitySets)
+    {
+        if (ensure(AbilitySet.IsValid()))
+        {
+            FGameplayAbilitySpec AbilitySpec{ AbilitySet.Ability, ApplyLevel };
+            AbilitySpec.SourceObject = InAbilitySystemComponent->GetAvatarActor();
+            AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.Tag);
+
+            InAbilitySystemComponent->GiveAbility(AbilitySpec);
+        }
+        else
+        {
+            // TODO: Report an error
+        }
+    }
+}
--- Source/Warrior/Public/DataAssets/StartUpData/DataAsset_HeroStartUpData.h
+++ Source/Warrior/Public/DataAssets/StartUpData/DataAsset_HeroStartUpData.h
@@ -2,10 +2,35 @@
 
 #include "CoreMinimal.h"
 #include "DataAssets/StartUpData/DataAsset_StartUpDataBase.h"
+#include "GameplayTagContainer.h"
 #include "DataAsset_HeroStartUpData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FWarriorHeroAbilitySet
+{
+    GENERATED_BODY()
+
+    /** Tag to identify the Ability. */
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+    FGameplayTag Tag;
+
+    /** The Ability. */
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    TSubclassOf<UWarriorGameplayAbility> Ability;
+
+    bool IsValid() const;
+};
 
 UCLASS()
 class WARRIOR_API UDataAsset_HeroStartUpData : public UDataAsset_StartUpDataBase
 {
     GENERATED_BODY()
+
+    /** Abilities that are granted to the hero at startup. */
+    UPROPERTY(EditDefaultsOnly, Category = "StartUpData", meta = (AllowAbstract = "false", TitleProperty = "Tag"))
+    TArray<FWarriorHeroAbilitySet> StartUpAbilitySets;
+
+public:
+    virtual void GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InAbilitySystemComponent,
+                                              int32 ApplyLevel) override;
 };
