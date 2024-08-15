c984d369af52ecd6668bb3d54d16fd4e66b574f9
Peter Donald <peter@realityforge.org>
2024-08-15 10:43:22 +1000

    Initialize the Aeon plugin

---
+++ Plugins/Aeon/Source/Aeon/Aeon.Build.cs
@@ -0,0 +1,26 @@
+using UnrealBuildTool;
+
+public class Aeon : ModuleRules
+{
+	public Aeon(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.AddRange( new string[] {} );
+		PrivateIncludePaths.AddRange( new string[] { } );
+		PublicDependencyModuleNames.AddRange(
+			new []
+			{
+				"Core",
+			}
+			);
+		PrivateDependencyModuleNames.AddRange(
+			new []
+			{
+				"CoreUObject",
+				"Engine",
+			}
+		);
+		DynamicallyLoadedModuleNames.AddRange( new string[] { } );
+	}
+}
---
+++ Plugins/Aeon/Source/Aeon/Private/Aeon.cpp
@@ -0,0 +1,15 @@
+#include "Aeon.h"
+
+#define LOCTEXT_NAMESPACE "FAeonModule"
+
+void FAeonModule::StartupModule()
+{
+}
+
+void FAeonModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FAeonModule, Aeon)
