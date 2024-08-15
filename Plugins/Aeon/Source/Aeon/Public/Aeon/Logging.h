56cf04f8146d5385630b052701b2f5dc479ca5f9
Peter Donald <peter@realityforge.org>
2024-08-15 11:01:04 +1000

    Start to setup logging infrastructure for Aeon

---
+++ Plugins/Aeon/Source/Aeon/Public/Aeon/Logging.h
@@ -0,0 +1,39 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "Logging/LogCategory.h"
+
+// Category usage:
+//	Warning/Error - Unexpected state or problem occured. Should be addressed.
+//	Log - What occurred? Aeon users care about this.
+//	Verbose - Why did it occur? Aeon developers care about this.
+//  VeryVerbose - What didn't occur and why did it NOT occur? Aeon developers care about this.
+//
+
+// Log category to use within the Aeon plugin
+AEON_API DECLARE_LOG_CATEGORY_EXTERN(Aeon, Display, All);
+
+// A simplified set of macros for logging
+
+#define AEON_LOG(Verbosity, Format, ...)                            \
+    {                                                               \
+        UE_LOG(LogAbilitySystem, Verbosity, Format, ##__VA_ARGS__); \
+    }
+
+#define AEON_ERROR_LOG(Format, ...) AEON_LOG(Warning, Format, ...)
+#define AEON_WARNING_LOG(Format, ...) AEON_LOG(Warning, Format, ...)
+#define AEON_INFO_LOG(Format, ...) AEON_LOG(Log, Format, ...)
+#define AEON_VERBOSE_LOG(Format, ...) AEON_LOG(Verbose, Format, ...)
+#define AEON_VERY_VERBOSE_LOG(Format, ...) AEON_LOG(VeryVerbose, Format, ...)
