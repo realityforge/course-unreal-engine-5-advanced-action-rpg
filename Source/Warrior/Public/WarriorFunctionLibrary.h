0131783d7689419e59700d095c0c8093ae9bd43f
Peter Donald <peter@realityforge.org>
2024-08-30 23:16:13 +1000

    Reorder natives

--- Source/Warrior/Public/WarriorFunctionLibrary.h
+++ Source/Warrior/Public/WarriorFunctionLibrary.h
@@ -24,19 +24,19 @@ class WARRIOR_API UWarriorFunctionLibrary : public UBlueprintFunctionLibrary
     GENERATED_BODY()
 
 public:
-    static UAeonAbilitySystemComponent* NativeGetAeonAbilitySystemComponentFromActor(AActor* InActor);
-
     UFUNCTION(BlueprintCallable, Category = "Warrior|FunctionLibrary")
     static void AddGameplayTagToActorIfNotPresent(AActor* InActor, FGameplayTag InGameplayTag);
 
     UFUNCTION(BlueprintCallable, Category = "Warrior|FunctionLibrary")
     static void RemoveGameplayTagFromActorIfPresent(AActor* InActor, FGameplayTag InGameplayTag);
 
-    static bool NativeIsGameplayTagPresentOnActor(AActor* InActor, FGameplayTag InGameplayTag);
+    static UAeonAbilitySystemComponent* NativeGetAeonAbilitySystemComponentFromActor(AActor* InActor);
 
     UFUNCTION(BlueprintCallable,
               Category = "Warrior|FunctionLibrary",
               meta = (DisplayName = "Is GameplayTag Present On Actor", ExpandEnumAsExecs = "OutConfirmType"))
     static void
     BP_IsGameplayTagPresentOnActor(AActor* InActor, FGameplayTag InGameplayTag, EWarriorConfirmType& OutConfirmType);
+
+    static bool NativeIsGameplayTagPresentOnActor(AActor* InActor, FGameplayTag InGameplayTag);
 };
