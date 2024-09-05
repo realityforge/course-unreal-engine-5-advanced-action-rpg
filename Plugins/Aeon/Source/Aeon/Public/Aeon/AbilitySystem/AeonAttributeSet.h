7b270717229ff492273e365cc6ba8fc66587fdbc
Peter Donald <peter@realityforge.org>
2024-09-05 14:24:45 +1000

    Define macro as recommended in AttributeSet.h

--- Plugins/Aeon/Source/Aeon/Public/Aeon/AbilitySystem/AeonAttributeSet.h
+++ Plugins/Aeon/Source/Aeon/Public/Aeon/AbilitySystem/AeonAttributeSet.h
@@ -7,6 +7,22 @@
 class UAeonAbilitySystemComponent;
 
 /**
+ * A macro to help define access to attributes.
+ * Recommended practice in AttributeSet.h (Why is it not defined there?)
+ *
+ * Use <code>ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, Health)</code> to define the functions:
+ *		static FGameplayAttribute GetHealthAttribute();
+ *		float GetHealth() const;
+ *		void SetHealth(float NewVal);
+ *		void InitHealth(float NewVal);
+ */
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName)           \
+    GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+    GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName)               \
+    GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName)               \
+    GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+/**
  * The base AttributeSet in Aeon.
  */
 UCLASS(Abstract, meta = (ShortTooltip = "The base AttributeSet used in Aeon."))
