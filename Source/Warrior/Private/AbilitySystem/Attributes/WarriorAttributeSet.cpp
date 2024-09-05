dd72eb90a1222d5ce3b9f1c896107c68b458cbd9
Peter Donald <peter@realityforge.org>
2024-09-05 15:05:36 +1000

    Define an AttributeSet that will be used in this project

--- Source/Warrior/Private/AbilitySystem/Attributes/WarriorAttributeSet.cpp
+++ Source/Warrior/Private/AbilitySystem/Attributes/WarriorAttributeSet.cpp
@@ -1,3 +1,11 @@
 #include "AbilitySystem/Attributes/WarriorAttributeSet.h"
 
-UWarriorAttributeSet::UWarriorAttributeSet() {}
+UWarriorAttributeSet::UWarriorAttributeSet()
+{
+    InitCurrentHealth(1.f);
+    InitMaxHealth(1.f);
+    InitCurrentRage(1.f);
+    InitMaxRage(1.f);
+    InitAttackPower(1.f);
+    InitDefensePower(1.f);
+}
--- Source/Warrior/Public/AbilitySystem/Attributes/WarriorAttributeSet.h
+++ Source/Warrior/Public/AbilitySystem/Attributes/WarriorAttributeSet.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "AbilitySystemComponent.h"
 #include "Aeon/AbilitySystem/AeonAttributeSet.h"
 #include "WarriorAttributeSet.generated.h"
 
@@ -11,6 +12,42 @@ class WARRIOR_API UWarriorAttributeSet : public UAeonAttributeSet
 {
     GENERATED_BODY()
 
+    /** The current health of the character. */
+    UPROPERTY(BlueprintReadOnly, Category = "Health", meta = (AllowPrivateAccess = "true"))
+    FGameplayAttributeData CurrentHealth;
+
+    /** The maximum health of the character. */
+    UPROPERTY(BlueprintReadOnly, Category = "Health", meta = (AllowPrivateAccess = "true"))
+    FGameplayAttributeData MaxHealth;
+
+    /** The current rage of the character. */
+    UPROPERTY(BlueprintReadOnly, Category = "Rage", meta = (AllowPrivateAccess = "true"))
+    FGameplayAttributeData CurrentRage;
+
+    /** The maximum rage of the character. */
+    UPROPERTY(BlueprintReadOnly, Category = "Rage", meta = (AllowPrivateAccess = "true"))
+    FGameplayAttributeData MaxRage;
+
+    /** The attack power of the character. */
+    UPROPERTY(BlueprintReadOnly, Category = "Damage", meta = (AllowPrivateAccess = "true"))
+    FGameplayAttributeData AttackPower;
+
+    /** The defense power of the character. */
+    UPROPERTY(BlueprintReadOnly, Category = "Damage", meta = (AllowPrivateAccess = "true"))
+    FGameplayAttributeData DefensePower;
+
+    /** The damage taken by the character. */
+    UPROPERTY(BlueprintReadOnly, Category = "Damage", meta = (AllowPrivateAccess = "true"))
+    FGameplayAttributeData DamageTaken;
+
 public:
     UWarriorAttributeSet();
+
+    ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, CurrentHealth)
+    ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, MaxHealth)
+    ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, CurrentRage)
+    ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, MaxRage)
+    ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, AttackPower)
+    ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, DefensePower)
+    ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, DamageTaken)
 };
