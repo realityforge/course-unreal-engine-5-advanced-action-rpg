79d471ec2717523203495d6f1810437f372845a4
Peter Donald <peter@realityforge.org>
2024-09-12 10:10:46 +1000

    Add callbacks for collision overlap start and end

--- Source/Warrior/Private/Items/Weapons/WarriorWeaponBase.cpp
+++ Source/Warrior/Private/Items/Weapons/WarriorWeaponBase.cpp
@@ -1,4 +1,5 @@
 #include "Items/Weapons/WarriorWeaponBase.h"
+#include "Aeon/Logging.h"
 #include "Components/BoxComponent.h"
 
 AWarriorWeaponBase::AWarriorWeaponBase()
@@ -12,4 +13,50 @@ AWarriorWeaponBase::AWarriorWeaponBase()
     WeaponCollisionBox->SetupAttachment(WeaponMesh);
     WeaponCollisionBox->SetBoxExtent(FVector(20.f));
     WeaponCollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    WeaponCollisionBox->OnComponentBeginOverlap.AddUniqueDynamic(this, &AWarriorWeaponBase::OnCollisionBoxBeginOverlap);
+    WeaponCollisionBox->OnComponentEndOverlap.AddUniqueDynamic(this, &AWarriorWeaponBase::OnCollisionBoxEndOverlap);
+}
+
+const APawn* AWarriorWeaponBase::GetPawnInstigator() const
+{
+    const auto PawnInstigator = GetInstigator<APawn>();
+    checkf(PawnInstigator,
+           TEXT("AWarriorWeaponBase::GetPawnInstigator invoked. "
+                "No instigator assigned as owner of the Weapon %s. Please fix."),
+           *GetName());
+    return PawnInstigator;
+}
+
+void AWarriorWeaponBase::OnCollisionBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+                                                    AActor* OtherActor,
+                                                    UPrimitiveComponent* OtherComp,
+                                                    int32 OtherBodyIndex,
+                                                    bool bFromSweep,
+                                                    const FHitResult& SweepResult)
+{
+    const auto PawnInstigator = GetPawnInstigator();
+    if (const auto HitPawn = Cast<APawn>(OtherActor))
+    {
+        if (PawnInstigator != HitPawn)
+        {
+            // You should not be able to hit yourself
+            AEON_SHOW_MESSAGE(FColor::Green, TEXT("%s BeginOverlap of %s"), *GetName(), *HitPawn->GetName());
+        }
+    }
+}
+
+void AWarriorWeaponBase::OnCollisionBoxEndOverlap(UPrimitiveComponent* OverlappedComponent,
+                                                  AActor* OtherActor,
+                                                  UPrimitiveComponent* OtherComp,
+                                                  int32 OtherBodyIndex)
+{
+    const auto PawnInstigator = GetPawnInstigator();
+    if (const auto HitPawn = Cast<APawn>(OtherActor))
+    {
+        if (PawnInstigator != HitPawn)
+        {
+            // You should not be able to hit yourself
+            AEON_SHOW_MESSAGE(FColor::Red, TEXT("%s EndOverlap of %s"), *GetName(), *HitPawn->GetName());
+        }
+    }
 }
--- Source/Warrior/Public/Items/Weapons/WarriorWeaponBase.h
+++ Source/Warrior/Public/Items/Weapons/WarriorWeaponBase.h
@@ -17,6 +17,22 @@ class WARRIOR_API AWarriorWeaponBase : public AActor
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapons", meta = (AllowPrivateAccess = "true"))
     TObjectPtr<UBoxComponent> WeaponCollisionBox{ nullptr };
 
+    const APawn* GetPawnInstigator() const;
+
+protected:
+    UFUNCTION()
+    virtual void OnCollisionBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+                                            AActor* OtherActor,
+                                            UPrimitiveComponent* OtherComp,
+                                            int32 OtherBodyIndex,
+                                            bool bFromSweep,
+                                            const FHitResult& SweepResult);
+    UFUNCTION()
+    virtual void OnCollisionBoxEndOverlap(UPrimitiveComponent* OverlappedComponent,
+                                          AActor* OtherActor,
+                                          UPrimitiveComponent* OtherComp,
+                                          int32 OtherBodyIndex);
+
 public:
     AWarriorWeaponBase();
 
