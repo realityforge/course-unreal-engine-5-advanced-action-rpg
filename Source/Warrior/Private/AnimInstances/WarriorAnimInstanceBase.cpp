cfbcdb66fb0b1660ed1288d6efcdde33bbb1060a
Peter Donald <peter@realityforge.org>
2024-10-02 11:11:42 +1000

    Add helper function for anim instances

--- Source/Warrior/Private/AnimInstances/WarriorAnimInstanceBase.cpp
+++ Source/Warrior/Private/AnimInstances/WarriorAnimInstanceBase.cpp
@@ -1,1 +1,15 @@
 #include "AnimInstances/WarriorAnimInstanceBase.h"
+#include "GameplayTagContainer.h"
+#include "WarriorFunctionLibrary.h"
+
+bool UWarriorAnimInstanceBase::IsTagPresentOnOwner(const FGameplayTag TagToCheck) const
+{
+    if (const auto OwningPawn = TryGetPawnOwner())
+    {
+        return UWarriorFunctionLibrary::NativeIsGameplayTagPresentOnActor(OwningPawn, TagToCheck);
+    }
+    else
+    {
+        return false;
+    }
+}
--- Source/Warrior/Public/AnimInstances/WarriorAnimInstanceBase.h
+++ Source/Warrior/Public/AnimInstances/WarriorAnimInstanceBase.h
@@ -4,8 +4,14 @@
 #include "CoreMinimal.h"
 #include "WarriorAnimInstanceBase.generated.h"
 
+struct FGameplayTag;
+
 UCLASS(Abstract)
 class WARRIOR_API UWarriorAnimInstanceBase : public UAnimInstance
 {
     GENERATED_BODY()
+
+protected:
+    UFUNCTION(BlueprintPure, meta = (BlueprintThreadSafe))
+    bool IsTagPresentOnOwner(FGameplayTag TagToCheck) const;
 };
